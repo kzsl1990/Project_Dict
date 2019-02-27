01e6fe8d56face3695db1976a016f6da946c6104
Wang Xiaobing <46618141+kzsl1990@users.noreply.github.com>
2019-02-27 16:24:43 +0800

    表字段更新
    表字段更新

--- LoadDict.py
+++ LoadDict.py
@@ -7,9 +7,9 @@ cur = db.cursor()
 
 cur.execute("drop table if exists DictTable;")
 cur.execute("create table DictTable \
-    (id int primary key, \
-    word char(30), \
-    Paraphrase varchar(500))charset=utf8;")
+    (id int primary key auto_increment, \
+    word char(32) not null, \
+    interpret text not null)charset=utf8;")
 i = 0
 with open(r'.\dict.txt') as f:
     while True:
@@ -19,11 +19,11 @@ with open(r'.\dict.txt') as f:
             break
         word = re.findall(r'^\S*', line)[0]
         word = word.replace("\'", "\'\'")
-        paraphrase = line[len(word):].lstrip()[:-1]
-        paraphrase = paraphrase.replace("\'", "\'\'")
-        paraphrase = paraphrase.replace("\\", "\\\\")
+        interpret = line[len(word):].lstrip()[:-1]
+        interpret = interpret.replace("\'", "\'\'")
+        interpret = interpret.replace("\\", "\\\\")
 
-        insert_info = '''insert into DictTable values (%d, '%s', '%s');''' % (i, word, paraphrase)
+        insert_info = '''insert into DictTable values (%d, '%s', '%s');''' % (i, word, interpret)
         # print(insert_info)
 
         cur.execute(insert_info)
--- UserData.py
+++ UserData.py
@@ -3,13 +3,15 @@
 import pymysql
 
 # 存入单个用户信息
-def restore_info(username, password):
+# 用户名已存在问题未处理
+def restore_info(username, password = '00000000'):
     db = pymysql.connect('172.16.10.124', 'root', 'root', database = 'MKWEB_PY', charset='utf8')
     cur = db.cursor()
 
     cur.execute("create table if not exists UserInfo \
-        (UserId char(20), \
-        password char(20), \
+        (id int primary key auto_increment, \
+        UserId varchar(32) not null, \
+        password char(16), \
         unique(UserId))charset=utf8;")
     insertinfo = "insert into UserInfo values ('%s', '%s');" % (username, password)
     cur.execute(insertinfo)
@@ -23,8 +25,12 @@ def restore_info(username, password):
 def reset_info():
     db = pymysql.connect('172.16.10.124', 'root', 'root', database = 'MKWEB_PY', charset='utf8')
     cur = db.cursor()
-
     cur.execute("delete from UserInfo;")
     db.commit()
     cur.close()
     db.close()  
+
+# reset_info()
+# restore_info('kzsl1990', '89085012')
+# restore_info('kzsl1991')
+# restore_info('kzsl1992', '89085012')
